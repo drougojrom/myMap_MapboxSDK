90379a7abd63de4d210541eac4ba1480e4a46ba9
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-05-23 15:36:14 +0300

    locations and directions

--- myMap_MapBoxSDK/MapViewController.swift
+++ myMap_MapBoxSDK/MapViewController.swift
@@ -11,34 +11,79 @@ import Mapbox
 import MapboxGeocoder
 import MapboxDirections
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MGLMapViewDelegate {
 
     @IBOutlet var mapView: MGLMapView!
     
     var arrayHelper = [String]()
     let geocoder = MBGeocoder(accessToken: "pk.eyJ1IjoiZHJvdWdvanJvbSIsImEiOiJjaW9janJkazcwMDV0dmptNWsxczRocTl0In0.w4hQl8U5yQdOqstMG5z6Xg")
     
+    var addressPlacemarkOne = MBPlacemark()
+    var addressPlacemarkTwo = MBPlacemark()
+    
+    let pointOne = MGLPointAnnotation()
+    let pointTwo = MGLPointAnnotation()
+    var myRoute: MBRoute?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let point = MGLPointAnnotation()
-        point.coordinate = CLLocationCoordinate2D(latitude: 55.760278, longitude: 37.618611)
-        point.title = "Большой театр"
-        point.subtitle = "Москва, Россия"
+        findLocations(pointOne, pointTwo: pointTwo)
         
-        let task = geocoder.geocodeAddressString(arrayHelper.first!, completionHandler: { (placemarks, error) in
+    }
+    
+    
+    // find locations using geocoding
+    func findLocations(pointOne: MGLPointAnnotation, pointTwo: MGLPointAnnotation) {
         
+        let taskOne = geocoder.geocodeAddressString(arrayHelper.first!, completionHandler: { (placemarks, error) in
+            
             
             placemarks?.count
-            let addressPlacemark = placemarks![0]
-            print(addressPlacemark)
+            self.addressPlacemarkOne = placemarks![0]
+            
+            pointOne.coordinate = CLLocationCoordinate2D(latitude: (self.addressPlacemarkOne.location?.coordinate.latitude)!, longitude: (self.addressPlacemarkOne.location?.coordinate.longitude)!)
+            pointOne.title = self.addressPlacemarkOne.name
+            pointOne.subtitle = self.addressPlacemarkOne.country
+            print(self.addressPlacemarkOne)
             print(placemarks)
+            self.mapView.addAnnotation(pointOne)
         })
         
-        print(task)
-        mapView.addAnnotation(point)
+        let taskTwo = geocoder.geocodeAddressString(arrayHelper.last!, completionHandler: { (placemarks , error) in
+            
+            placemarks?.count
+            self.addressPlacemarkTwo = placemarks![0]
+            pointTwo.coordinate = CLLocationCoordinate2D(latitude: (self.addressPlacemarkTwo.location?.coordinate.latitude)!, longitude: (self.addressPlacemarkTwo.location?.coordinate.longitude)!)
+            pointTwo.title = self.addressPlacemarkTwo.name
+            pointTwo.subtitle = self.addressPlacemarkTwo.country
+            print(self.addressPlacemarkTwo)
+            self.mapView.addAnnotation(pointTwo)
+        })
         
+        
+        print(taskOne)
+        print(taskTwo)
+        
+    }
+    
+    func mapView(mapView: MGLMapView, lineWidthForPolylineAnnotation annotation: MGLPolyline) -> CGFloat {
+        return 5
+    }
+    
+    @IBAction func drawRoute(sender: UIBarButtonItem) {
+        
+        let directionsRequest = MBDirectionsRequest(sourceCoordinate: pointOne.coordinate, destinationCoordinate: pointTwo.coordinate)
+        directionsRequest.transportType = .Automobile
+        
+        let directions = MBDirections(request: directionsRequest, accessToken: "pk.eyJ1IjoiZHJvdWdvanJvbSIsImEiOiJjaW9janJkazcwMDV0dmptNWsxczRocTl0In0")
+        
+        
+    }
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
